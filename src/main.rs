660ea8f4d0505630eb6f24e3e51e4fe32db3d7b3
onebitbool <onebitbool@gmail.com>
2023-03-16 19:25:12 +0800

    Add: basic framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    env, fs,
+    io::{self, Write},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(args.get(1).unwrap()),
+        _ => panic!("Usage: jlox [script]"),
+    }
+}
+
+fn run_prompt() {
+    let stdin = io::stdin();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        match stdin.read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => run(line),
+            Err(err) => panic!("Error: {}", err),
+        }
+    }
+}
+
+fn run_file(file_name: &str) {
+    let content = fs::read_to_string(file_name).expect("Error: Wrong path");
+    run(content);
+}
+
+fn run(content: String) {
+    println!("Run: {}", content);
 }
