cc50fa4dd2256738794462028c170eae522bbb79
onebitbool <onebitbool@gmail.com>
2023-03-19 14:42:25 +0800

    Add: token file

--- src/token.rs
+++ src/token.rs
@@ -1,20 +1,63 @@
+#[derive(Debug, PartialEq)]
 pub enum TokenType {
     // Single-character tokens
-    LeftParen, RightParen, LeftBrace, RightBrace,
-    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
 
     // One or two character tokens
-    Bang, BangEqual,
-    Equal, EqulaEqual,
-    Greater, GreaterEqual,
-    Less, LessEqual,
+    Bang,
+    BangEqual,
+    Equal,
+    EqulaEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
     // Literals
-    Identifier, String, Number,
-    
+    Identifier,
+    String,
+    Number,
+
     // Keywords
-    And, Class, Else, False,For, Fun, If, Nil, Or,
-    Print, Return, Super, This, True, Var, While,
-    
-    Eof
+    And,
+    Class,
+    Else,
+    False,
+    For,
+    Fun,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
+
+#[derive(Debug)]
+pub enum Lexeme<'a> {
+    String(&'a str),
+    Number(i32),
+}
+
+#[derive(Debug)]
+pub struct Token<'a> {
+    pub token_type: TokenType,
+    pub line: u32,
+    pub lexeme: Lexeme<'a>,
 }
